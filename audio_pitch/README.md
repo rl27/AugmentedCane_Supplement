9d1d6cac1221e9d9c9614aefd9b6620be6a41246
rl27 <raymond.liu1234@gmail.com>
2024-02-09 15:38:19 -0500

    Audio pitch

---
+++ audio_pitch/README.md
@@ -0,0 +1,3 @@
+`pitch.py` uses ffmpeg to generate pitched-down versions of audio files while retaining the same audio speed/length.
+
+I ran this on Windows with `ffmpeg.exe` accessible through my PATH environment variable (i.e. I can use `ffmpeg` directly in commands).
---
+++ audio_pitch/pitch.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+import numpy as np
+
+# https://stackoverflow.com/a/53376979
+
+# Filenames: a.wave, a+.wav, b.wav
+original_audio = ['a', 'a+', 'b']
+
+# Pitches to generate
+pitches = np.arange(0.2, 1, 0.02)
+
+# Assume a sampling rate of 44100 Hz
+command = '"ffmpeg -i {0}.wav -af asetrate=44100*{1},aresample=44100,atempo=1/{1} {0}/{1}.wav"'
+
+for n in original_audio:
+	if os.path.isdir(n):
+		shutil.rmtree(n)
+	os.makedirs(n)
+
+	# Generate pitched-down files
+	for i in pitches:
+		os.system(command.format(n, format(i, '.2f')))
+
+	# Copy over original file
+	shutil.copy('{}.wav'.format(n), '{}/{}.wav'.format(n, '1.00'))
